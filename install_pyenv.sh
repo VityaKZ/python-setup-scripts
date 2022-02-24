9d36b43e92e110a8de86847df8f44bf0bb7cc779
BrandonLWhite <brandonlwhite@gmail.com>
2022-02-24 09:11:11 -0600

    Initial implementation

---
+++ install_pyenv.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# TODO BW: Update this
+# To execute this from a command prompt:
+# curl https://gist.githubusercontent.com/BrandonLWhite/41b2a16487eef8abd193d9481dfd37f3/raw | bash
+
+# Set bash as the default shell.
+chsh -s /bin/bash
+
+# Install pyenv
+curl https://pyenv.run | bash
+
+# Add pyenv to path
+# If this issue (https://github.com/pyenv/pyenv-installer/issues/112) is ever implemented we can hopefully remove this
+# manual addition.
+echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.profile
+echo 'eval "$(pyenv init --path)"' >> ~/.profile
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+source ~/.profile
+
+# Add the pyenv-alias-latest-patch-version so we can reference minor versions without specific patch versions
+git clone https://github.com/upside-services/pyenv-alias-latest-patch-version.git $(pyenv root)/plugins/pyenv-alias-latest-patch-version
+
+# Install all the versions we want
+pyenv install 3.6:latest
+pyenv install 3.7:latest
+pyenv install 3.8:latest
+pyenv install 3.9:latest
+
+# Set the default version to 3.6
+# This lets 3.6 be ambient for projects that don't yet have .python-version.
+pyenv global 3.6
